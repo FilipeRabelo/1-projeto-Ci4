223f5b64ab66bdf250fc92bc34f258d5e97b5cb0
FILIPE RABELO FIGUEIREDO LANA <91102847+FilipeRabelo@users.noreply.github.com>
2022-04-18 16:52:05 -0300

    Primeiro projeto CI4

---
+++ app/Controllers/Clientes.php
@@ -0,0 +1,20 @@
+<?php
+
+    namespace App\Controllers;
+
+    use CodeIgniter\Controller;
+
+    class Clientes extends Controller{
+
+        public function novo(){
+
+           
+            echo View("templates/header");
+            echo View("clientes/novo");
+            echo View("templates/footer");
+
+        }
+
+    }
+
+?>
