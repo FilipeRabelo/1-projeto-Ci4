b4227db023ff1ff855a8d16ed24edeb4969bf2fa
FILIPE RABELO FIGUEIREDO LANA <91102847+FilipeRabelo@users.noreply.github.com>
2022-04-17 22:14:12 -0300

    Primeiro projeto CI4

--- app/Controllers/Inicio.php
+++ app/Controllers/Inicio.php
@@ -1,23 +1,20 @@
-
-<!-- ARQUIVO PARA CHAMAR OS TEMPLATES COM AS VIEWS SEPERADAS PELO MVC -->
-
-<?php
-
+<?php    
+    
     namespace App\Controllers;
 
     use CodeIgniter\Config\View;
     use CodeIgniter\Controller;
 
-    class Inicio extends Controller{
+    class Inicio extends Controller
+    {
 
-        public function index(){
+        public function index()
+        {
 
             echo View("templates/header");
             echo View("inicio/index");
-            echo View("templates/footer");           
-
+            echo View("templates/footer");
         }
-
     }
 
 ?>
