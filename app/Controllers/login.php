c054fb4fedb33cd49e6fc612525a9fa87bae2a24
FILIPE RABELO FIGUEIREDO LANA <91102847+FilipeRabelo@users.noreply.github.com>
2022-04-24 18:30:42 -0300

    Primeiro projeto CI4

--- app/Controllers/login.php
+++ app/Controllers/login.php
@@ -1,14 +1,56 @@
 <?php
 
   namespace App\Controllers;
-
+  
+  use App\Models\LoginModel;
   use CodeIgniter\Controller;
 
   class Login extends Controller{
 
+    private $login_model;
+
+    function __construct(){
+
+      $this->login_model = new LoginModel();
+      
+    }
+
     public function index(){   
       
       echo View("login/index");     
+
+    }
+
+    // AUTENTIFICAÇÃO //
+
+    public function autenticar(){
+      
+      $dados = $this->request->getVar();       // RECUPERANDO TODOS OS DADOS Q ESTAO VINDO DO FORM
+
+      $usuario = $this->login_model
+                      ->where("usuario", $dados["usuario"])
+                      ->where( "senha" , $dados["senha"])  // VERIFICANDO SE DADOS CORRESPONDEN AOS DADOS CADASTRADOS NO BD
+                      ->first();
+
+      //SE EXISTIR USUARIO
+
+      $session = session();
+
+      if(!empty($usuario)):  // empty vazio // SE A VARIAVEL EXISTE CRIA A SESSAO
+
+        $session->set("primeiro_nome", $usuario["primeiro_nome"]);
+
+        $session->setFlashdata("alert", "sucess_login");
+
+        return redirect()->to("http://localhost/curso-Ci4/1-projeto-Ci4/public/inicio");
+
+      endif;
+
+      //SE NAO EXISTIR USUARIO
+
+      $session->setFlashdata("alert", "erro_login");
+
+      return redirect()->to("http://localhost/curso-Ci4/1-projeto-Ci4/public/login");
 
     }
 
--- app/Views/login/index.php
+++ app/Views/login/index.php
@@ -1,82 +1,83 @@
 <!DOCTYPE html>
 <html lang="pt-br">
 
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>All Tech</title>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>All Tech</title>
 
-  <!-- Google Font: Source Sans Pro -->
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <!-- Google Font: Source Sans Pro -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
 
-  <!-- Font Awesome -->
+    <!-- Font Awesome -->
 
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
-  <!-- icheck bootstrap -->
-  <link rel="stylesheet" href="<?= base_url("theme/plugins/icheck-bootstrap/icheck-bootstrap.min.css") ?>">
+    <!-- icheck bootstrap -->
+    <link rel="stylesheet" href="<?= base_url("theme/plugins/icheck-bootstrap/icheck-bootstrap.min.css") ?>">
 
-  <!-- Theme style -->
-  <link rel="stylesheet" href="<?= base_url("theme/dist/css/adminlte.min.css") ?>">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="<?= base_url("theme/dist/css/adminlte.min.css") ?>">
 
-</head>
+  </head>
 
-<body class="hold-transition login-page" style="background-color: #17a2b8;">
-  <div class="login-box">
-    <!-- /.login-logo -->
-    <div class="card card-outline card-primary">
+  <body class="hold-transition login-page" style="background-color: #17a2b8;">
+    <div class="login-box">
+      <!-- /.login-logo -->
+      <div class="card card-outline card-primary">
 
-      <div class="card-header text-center">
-        <a href="../../index2.html" class="h1">All Tech</a>
-      </div>
+        <div class="card-header text-center">
+          <a href="../../index2.html" class="h1">All Tech</a>
+        </div>
 
-      <div class="card-body">
+        <div class="card-body">
 
-        <p class="login-box-msg">Entre com seu usuário e senha</p>
+          <p class="login-box-msg">Entre com seu usuário e senha</p>
 
-        <form action="http://localhost/curso-Ci4/1-projeto-Ci4/public/login/autenticar" method="post">
+          <form action="http://localhost/curso-Ci4/1-projeto-Ci4/public/login/autenticar" method="post">
 
-          <div class="input-group mb-3">
-            <input type="text" class="form-control" placeholder="Usuário" required>
-            <div class="input-group-append">
-              <div class="input-group-text">
-                <span class="fas fa-user"></span>
+            <div class="input-group mb-3">
+              <input type="text" class="form-control" name="usuario" placeholder="Usuário" required>
+              <div class="input-group-append">
+                <div class="input-group-text">
+                  <span class="fas fa-user"></span>
+                </div>
               </div>
             </div>
-          </div>
 
-          <div class="input-group mb-3">
-            <input type="password" class="form-control" placeholder="Senha" required>
-            <div class="input-group-append">
-              <div class="input-group-text">
-                <span class="fas fa-lock"></span>
+            <div class="input-group mb-3">
+              <input type="password" class="form-control" name="senha" placeholder="Senha" required>
+              <div class="input-group-append">
+                <div class="input-group-text">
+                  <span class="fas fa-lock"></span>
+                </div>
               </div>
             </div>
-          </div>
 
-          <div class="row">
-            <!-- /.col -->
-            <div class="col-12">
-              <button type="submit" class="btn btn-primary btn-block">Acessar</button>
+            <div class="row">
+              <!-- /.col -->
+              <div class="col-12">
+                <button type="submit" class="btn btn-primary btn-block">Acessar</button>
+              </div>
+              <!-- /.col -->
             </div>
-            <!-- /.col -->
-          </div>
 
-        </form>
+          </form>
 
+        </div>
+        <!-- /.card-body -->
       </div>
-      <!-- /.card-body -->
+      <!-- /.card -->
     </div>
-    <!-- /.card -->
-  </div>
-  <!-- /.login-box -->
+    <!-- /.login-box -->
 
-  <!-- jQuery -->
-  <script src="<?= base_url("theme/plugins/jquery/jquery.min.js") ?>"></script>
-  <!-- Bootstrap 4 -->
-  <script src="<?= base_url("theme/plugins/bootstrap/js/bootstrap.bundle.min.js") ?>"></script>
-  <!-- AdminLTE App -->
-  <script src="<?= base_url("theme/dist/js/adminlte.min.js") ?>"></script>
-</body>
+    <!-- jQuery -->
+    <script src="<?= base_url("theme/plugins/jquery/jquery.min.js") ?>"></script>
+    <!-- Bootstrap 4 -->
+    <script src="<?= base_url("theme/plugins/bootstrap/js/bootstrap.bundle.min.js") ?>"></script>
+    <!-- AdminLTE App -->
+    <script src="<?= base_url("theme/dist/js/adminlte.min.js") ?>"></script>
+    
+  </body>
 
 </html>
